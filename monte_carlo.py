09d448b7706e74062a914a0be354e07e7aa3e39e
Adam Simpson <atj@atjlt2.ornl.gov>
2014-03-18 15:38:47 -0400

    modified monte_carlo

--- monte_carlo.py
+++ monte_carlo.py
@@ -2,7 +2,7 @@ from mpi4py import MPI
 import numpy
 import scipy
 import matplotlib.pyplot as pyplot
-from matplotlib.patches import Rectangle
+from matplotlib.patches import Circle
 from scipy import integrate
 
 def func(x) :
@@ -20,11 +20,7 @@ def main() :
     xEnd = 2.0 * scipy.pi
 
     # Number of total samples
-    samplesPerRank = 10
-
-    # Node start and end
-    rankStart = xStart + width*samplesPerRank*rank
-    rankEnd   = rankStart + width*samplesPerRank
+    samplesPerRank = 10000
 
     # Create x,y pair array to hold each nodes work
     coordinates = numpy.empty([samplesPerRank, 2])
@@ -41,19 +37,21 @@ def main() :
         totalInside = None
 
     # Create random points
-    yMin = func(xStart)
-    yMax = func(xEnd)
+    yMin = -1.0
+    yMax = 1.0
 
-    xValues = numpy.random.uniform(rankStart, rankEnd, samplesPerRank)
-    yValues = numpy.random.uniform(xMin, xMax, samplesPerRank)
+    xValues = numpy.random.uniform(xStart, xEnd, samplesPerRank)
+    yValues = numpy.random.uniform(yMin, yMax, samplesPerRank)
 
     # Fill x,y coordinate pairs and find number inside
     for i in range(0, samplesPerRank) :
         x = xValues[i] 
         y = yValues[i]
         coordinates[i] = [x, y]
-        if y <= func(x) :
+        if y < func(x) and y > 0:
             inside += 1
+        if y > func(x) and y < 0:
+            inside -= 1
 
     # Gather all coordinates on rank 0
     MPI.COMM_WORLD.Gather(coordinates, allCoordinates, root=0)    
@@ -66,24 +64,26 @@ def main() :
         # Add subplot to place rectangles into
         ax = pyplot.figure().add_subplot(1,1,1)
       
-        # Plot of all points
-        pyplot.plot(*zip(*allCoordinates))
+        # Plot of function
+        xSamples = numpy.arange(xStart, xEnd, 0.1);
+        vecFunc = numpy.vectorize(func)
+        ySamples = vecFunc(xSamples)
+        pyplot.plot(xSamples, ySamples)
 
-        # Plot rectangles
+        # Plot random points
         for i in range(0, samplesPerRank*size) :
-            lowerLeft = allCoordinates[i][0]
-            height = allCoordinates[i][1]
-
             # Change color based on node
             if i%samplesPerRank == 0 :
                 randColor = numpy.random.rand(3, 1)
 
-            ax.add_patch(Rectangle((lowerLeft,0), width, height, facecolor=randColor, edgecolor='black') )
+            ax.add_patch(Circle(allCoordinates[i], 0.03, color=randColor) )
 
         # Calculate integral
         actualArea = integrate.quad(func, xStart, xEnd)[0]
 
-        # Calculate difference 
+        # Calculate difference
+        totalPoints = samplesPerRank*size
+        totalArea = (xEnd - xStart)*(yMax-yMin)*totalInside/totalPoints
         error = actualArea - totalArea
 
         # Add annotation to show total area
